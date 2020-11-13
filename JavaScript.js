4c54697bd37ac0f8069552e815384e34b6cfcac1
Manas Tripathi <tripathimanas98@gmail.com>
2020-11-14 03:47:57 +0530

    stacks completed

--- JavaScript.js
+++ JavaScript.js
@@ -57,6 +57,8 @@ var manas = new Hobby("Manas", 22, "Web developer", "cooking");
 // reduce()
 // map()
 // filter()
+// splice()
+// slice()
 
 //! testing of each function
 // var arr = new Array(1, 9, 13, 6, 4);
@@ -72,6 +74,9 @@ var manas = new Hobby("Manas", 22, "Web developer", "cooking");
 // console.log(arr.reduce((acc, item) => acc + item)); // uses an accumulator, reduces the entire array into a single element
 // console.log(arr.map((item) => item + 5)); // performs certain task on each element of the array and returns a new array
 // console.log(arr.filter((item) => item % 2 == 0)); // returns a new array with only the elements which return "true" for the callback function within filter function
+// console.log(arr.splice(1, 2)); // mutates the original array, and returns the spliced array
+// console.log(arr.slice(1, 3)); // returns the sliced array, takes starting and ending position
+// console.log([...arr.slice(0, 2), ...arr.slice(3, 5)]); //remove a specific item
 // console.log(arr);
 
 // ! Jagged array is a multidimensional array where the lengths of rows are not uniform
@@ -95,6 +100,7 @@ var manas = new Hobby("Manas", 22, "Web developer", "cooking");
 // console.log(count, totalArray);
 
 //! book page 33 question 1
+// ! Create a grades object that stores a set of student grades in an object. Provide a function for adding a grade and a function for displaying the student’s grade average.
 // class Grades {
 //   constructor() {
 //     this.gradesArray = [];
@@ -118,3 +124,201 @@ var manas = new Hobby("Manas", 22, "Web developer", "cooking");
 // }
 
 // var manas = new Grades();
+
+//! book page 33 question 2
+//! Store a set of words in an array and display the contents both forward and backward
+
+// let arr = ["manas", "is", "a", "programmer"];
+// console.log(arr);
+// console.log(arr.reverse());
+
+//! book page 33 question 4
+// ! Create an object that stores individual letters in an array and has a function for displaying the letters as a single word.
+
+// let string = "hello there, how are you?";
+// let arr = string.split(" ").join("").split(""); // remove space, join words, split letters
+// console.log(arr);
+// let joined = arr.join("");
+// console.log(joined);
+
+// ! Reading a txt file and working with data
+// var movies = "";
+// fetch("movies.txt")
+//   .then((Response) => Response.text())
+//   .then((Response) => console.log(Response.split(",")));
+
+// // console.log(movies);
+
+// ! Page 47 question 3
+// ! Create a Person class that stores a person’s name and their gender. Create a list of at least 10 Person objects. Write a function that displays all the people in the list of the same gender.
+
+// class Person {
+//   constructor(name, gender) {
+//     (this.name = name), (this.gender = gender);
+//   }
+// }
+
+// class PersonList {
+//   constructor() {
+//     this._dataStore = [];
+//   }
+//   append = (element) => {
+//     this._dataStore.push(element);
+//   };
+//   showMale = () => {
+//     for (let item of this._dataStore) {
+//       if (item.gender == "male") {
+//         console.log(item);
+//       } else {
+//         continue;
+//       }
+//     }
+//     return null;
+//   };
+//   showFemale = () => {
+//     for (let item of this._dataStore) {
+//       if (item.gender == "female") {
+//         console.log(item);
+//       } else {
+//         continue;
+//       }
+//     }
+//     return null;
+//   };
+// }
+
+// var genderShower = new PersonList();
+// genderShower.append(new Person("manas", "male"));
+// genderShower.append(new Person("adya", "female"));
+// genderShower.append(new Person("ajai", "male"));
+// genderShower.append(new Person("punita", "female"));
+
+// ! Checking palindrome of a number using STACK datastructure
+
+// class PalindromeStack {
+//   constructor() {
+//     this._dataStore = [];
+//     this.top = 0;
+//     this.length = 0;
+//   }
+//   push = (element) => {
+//     this._dataStore.push(element);
+//     ++this.length;
+//     ++this.top;
+//   };
+//   pop = () => {
+//     --this.top;
+//     --this.length;
+//     return this._dataStore.pop();
+//   };
+//   peek = () => {
+//     if (this.length) {
+//       console.log(this._dataStore[this.top - 1]);
+//     }
+//   };
+// }
+
+// let checker = new PalindromeStack();
+// let testWord1 = "racecar";
+// let testWord2 = "manas";
+
+// isPalindrome = (testCase) => {
+//   for (let letter of testCase) {
+//     checker.push(letter);
+//   }
+//   var dummyArray = [];
+//   let counter = checker.length;
+//   for (let i = 0; i < counter; i++) {
+//     dummyArray.push(checker.pop());
+//   }
+//   if (testCase == dummyArray.join("")) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+// console.log(isPalindrome(testWord1));
+
+// ! Factorial of a number using STACK
+
+// class FactorialStack {
+//   constructor() {
+//     this._dataStore = [];
+//     this.top = 0;
+//     this.length = 0;
+//   }
+//   push = (element) => {
+//     this._dataStore.push(element);
+//     ++this.length;
+//     ++this.top;
+//   };
+//   pop = () => {
+//     --this.top;
+//     --this.length;
+//     return this._dataStore.pop();
+//   };
+// }
+
+// factorialFinder = (num) => {
+//   let fact = new FactorialStack();
+//   let pushes = num;
+//   for (let i = 0; i < pushes; i++) {
+//     fact.push(num);
+//     --num;
+//   }
+//   let runningProduct = 1;
+//   let length = fact.length;
+//   for (let i = 0; i < length; i++) {
+//     runningProduct *= fact.pop();
+//   }
+//   return runningProduct;
+// };
+
+// console.log(factorialFinder(7));
+
+// ! Checking whether the bracketpairs are balanced or not
+
+class BracketStack {
+  constructor() {
+    this._dataStore = [];
+    this.length = 0;
+    this.top = 0;
+  }
+  push = (item) => {
+    ++this.length;
+    ++this.top;
+    this._dataStore.push(item);
+  };
+  pop = () => {
+    --this.length;
+    --this.top;
+    return this._dataStore.pop();
+  };
+}
+var bracket = new BracketStack();
+bracketBalanceChecker = (string) => {
+  let correctObj = {
+    "{": "}",
+    "[": "]",
+    "(": ")",
+  };
+  for (let letter of string) {
+    if (letter == "(" || letter == "[" || letter == "{") {
+      bracket.push(letter);
+    } else if (letter == ")" || letter == "]" || letter == "}") {
+      if (correctObj[bracket.pop()] != letter) {
+        return false;
+      }
+    }
+  }
+
+  return bracket.length ? false : true;
+};
+// ! test 1
+console.log(
+  bracketBalanceChecker(
+    "[{()()}({[]})]({}[({})])((((((()[])){}))[]{{{({({({{{{{{}}}}}})})})}}}))[][][]"
+  )
+); // true
+// ! test 2
+console.log(bracketBalanceChecker("({(()))}}")); // false
--- dsa.js
+++ dsa.js
@@ -1,8 +1,8 @@
-// ! *****************************************
+// ! ***************************************************************************
 // ! The code structure in this file
 // * Topic name
 // * Examples
-// ! *****************************************
+// ! ***************************************************************************
 
 // ! Recursion
 // * The  function calls itself within the definition. The function MUST have a base condition to avoid an infinite loop. The function goes through backtracking after the final/base condition is fulfilled
@@ -16,3 +16,143 @@ factorial = (num) => {
   }
 };
 // console.log(factorial(5));
+
+// ! ***************************************************************************
+// ! List
+// * The list is a basic datastructure found in any programming language. The numebr of elements in a list determines the length of a list. We can perform multiple tasks like add, remove, read, insert, print, check within the list, etc.
+
+// ! implementation of a List in JavaScript
+class List {
+  // ? List has 2 properties, length and position, _dataStore is an array to store our data
+  constructor() {
+    (this.length = 0), (this.position = 0), (this._dataStore = []);
+  }
+  // ? clears all the contents of the list
+  clear = () => {
+    this._dataStore = [];
+    this.length = 0;
+    this.position = 0;
+  };
+  // ? console logs the contents of our list
+  print = () => {
+    console.log(...this._dataStore);
+  };
+  // ? Returns the element at a specific requested position, returns "not found" if position is empty
+  getElement = (position) => {
+    if (this._dataStore[position] == undefined) {
+      console.warn("not found");
+    } else {
+      this.position = position;
+      return this._dataStore[position];
+    }
+  };
+  // ? add an element at the end of a List
+  append = (element) => {
+    this._dataStore[this.length] = element;
+    this.length += 1;
+    return "element added";
+  };
+  // ? Remove an element from a specific position in a list
+  remove = (position) => {
+    let arr = [
+      ...this._dataStore.slice(0, position),
+      ...this._dataStore.slice(position + 1, this.length),
+    ];
+    this._dataStore = arr;
+    this.length -= 1;
+    return "element removed";
+  };
+  // ? Insert an element at any position (not beyond bounds) in the list
+  insert = (element, position) => {
+    if (!this._dataStore[position]) {
+      this._dataStore.splice(position, 0, element);
+      this.length += 1;
+      return `element${element} inserted at ${position}`;
+    } else {
+      console.warn("cannot insert beyond bounds of existing list");
+    }
+  };
+  // ? Shows the front of a list (first element)
+  front = () => {
+    // console.log(this._dataStore[0]);
+    return this._dataStore[0];
+  };
+  // ? Shows the front of a list (first element)
+  end = () => {
+    // console.log(this._dataStore[this.length - 1]);
+    return this._dataStore[this.length - 1];
+  };
+  // ? Returns the element at previous position
+  previous = () => {
+    if (this.position == 0) {
+      console.warn("first element reached");
+    } else {
+      this.position -= 1;
+      return this._dataStore[this.position];
+    }
+  };
+  // ? Returns the element at next position
+  next = () => {
+    if (this.position == this.length - 1) {
+      console.warn("Last element reached");
+    } else {
+      this.position += 1;
+      return this._dataStore[this.position];
+    }
+  };
+  // ? Move the current position within bounds
+  movePosition = (position) => {
+    -1 < position && position < this.length - 1
+      ? (this.position = position)
+      : console.warn("list does not have this position");
+  };
+}
+
+var groceries = new List();
+
+// ! ***************************************************************************
+// ! STACK
+// * One of the most basic and fundamental datastructure. The stack works in LIFO manner and mimics a pile of books, stack of trays, or most importantly - the call stack
+
+// ! Implementation of a stack
+
+class Stack {
+  constructor() {
+    this._dataStore = [];
+    this.top = 0;
+    this.length = 0;
+  }
+  push = (element) => {
+    this._dataStore.push(element);
+    ++this.length;
+    ++this.top;
+  };
+  pop = () => {
+    --this.top;
+    --this.length;
+    return this._dataStore.pop();
+  };
+  peek = () => {
+    if (this.length) {
+      console.log(this._dataStore[this.top - 1]);
+    }
+  };
+  clear = () => {
+    this._dataStore = [];
+    this.length = 0;
+    this.top = 0;
+  };
+}
+
+let books = new Stack();
+
+
+// ! ***************************************************************************
+// ! QUEUE
+// * QUEUE is another most important datastructure. This works on a FIFO model. Basically like a queue at a grocery store during the lockdown.
+
+class Queue{
+  constructor() {
+    
+  }
+}
