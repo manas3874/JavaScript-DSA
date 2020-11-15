fe5aff7126c3ecfbd63c919970ad70711eb7f8ad
Manas Tripathi <tripathimanas98@gmail.com>
2020-11-16 01:29:51 +0530

    Set DS methods implemented by myself

--- dsa.js
+++ dsa.js
@@ -609,7 +609,6 @@ class CircularDoublyLinkedList {
     }
   };
 
-  
   advanceFrom = (startingNode, steps) => {
     var currentNode = this.find(startingNode);
     if (steps > 0) {
@@ -634,3 +633,129 @@ cdll.insertAfter("ajai", "saloni");
 cdll.print();
 // console.log(cdll.advance(3));
 console.log(cdll.advanceFrom("manas", 2));
+
+// ! SETS
+// ! Set is a mathematical property/ a group of items with no repetitions
+// ! ES6 gives us the Set object but we can implement our own with the most important union, intersection and difference methods
+
+class Set {
+  constructor() {
+    this.items = {};
+  }
+  has = (item) => {
+    return item in this.items;
+  };
+
+  add = (item) => {
+    if (!this.has(item)) {
+      this.items[item] = item;
+      return true;
+    }
+    return false;
+  };
+  delete = (item) => {
+    if (this.has(item)) {
+      delete this.items[item];
+      return true;
+    }
+    return false;
+  };
+  clear = () => {
+    this.items = {};
+  };
+  print = () => {
+    var arr = [];
+    for (let item in this.items) {
+      arr.push(item);
+    }
+    console.log(arr);
+  };
+
+  // ! This method gives us the union of two sets WITH repetition
+  // ! This method is used later in union/intersection methods accordingly
+  __unionSetArr = (set) => {
+    var arrOfArgument = [];
+    for (let item in set.items) {
+      arrOfArgument.push(item);
+    }
+    var arrOfLocal = [];
+    for (let item in this.items) {
+      arrOfLocal.push(item);
+    }
+    var unionArr = [...arrOfLocal, ...arrOfArgument];
+
+    return unionArr;
+  };
+
+  // ! This method gives the union of two sets
+  // ! The keys of the object are returned as an array
+  union = (set) => {
+    var unionSet = {};
+    var unionArr = this.__unionSetArr(set);
+    for (let item of unionArr) {
+      if (!(item in unionSet)) {
+        unionSet[item] = item;
+      } else {
+        continue;
+      }
+    }
+    return Object.keys(unionSet);
+  };
+  // ! This method gives the intersection of two arrays
+  // ! takes an optional argument which is set to false by default. This argument, when set to true, will give us the difference of two sets, which is passed to the difference method
+  intersection = (set, diff = false) => {
+    var unionArr = this.__unionSetArr(set);
+    var intersectionSet = {};
+    for (let item of unionArr) {
+      if (item in intersectionSet) {
+        intersectionSet[item] += 1;
+      } else {
+        intersectionSet[item] = 1;
+      }
+    }
+
+    if (!diff) {
+      // ! will return the intersection array
+      var intersectionArr = [];
+      for (let item in intersectionSet) {
+        if (intersectionSet[item] > 1) {
+          intersectionArr.push(item);
+        }
+      }
+      return intersectionArr;
+    } else {
+      //! will return the difference array
+      var differenceArr = [];
+      for (let item in intersectionSet) {
+        if (intersectionSet[item] == 1) {
+          differenceArr.push(item);
+        }
+      }
+      return differenceArr;
+    }
+  };
+
+  difference = (set) => {
+    return this.intersection(set, true);
+  };
+}
+
+var set1 = new Set();
+var set2 = new Set();
+set1.add(1);
+set1.add(2);
+set1.add(3);
+set1.add(6);
+set1.add(8);
+
+set2.add(4);
+set2.add(5);
+set2.add(6);
+set2.add(7);
+set2.add(8);
+
+// set1.print();
+// set2.print();
+console.log(set1.union(set2));
+console.log(set1.intersection(set2));
+console.log(set1.difference(set2));
