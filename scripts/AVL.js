fa11319b0647a1a35ee7750010fdfe33208d2e0f
Manas <tripathimanas98@gmail.com>
2020-11-25 00:37:30 +0530

    found position to perform rotation in AVL tree

--- scripts/AVL.js
+++ scripts/AVL.js
@@ -5,18 +5,41 @@ export class AVL extends BinarySearchTree {
     super();
   }
   insertNode = (data) => {
-    var insertedNodeParent = this.insert(data).parent;
+    var insertedNode = this.insert(data);
     // console.log(this.insert(data));
-    // console.log(insertedNodeParent);
-    console.log(this.__balanceFactor(insertedNodeParent));
+    console.log(insertedNode);
+    var currentNode = insertedNode;
+    while (currentNode != this.root) {
+      if (this.__balanceFactor(currentNode) == -2) {
+        console.log(currentNode.data + " is right heavy");
+        currentNode.right.right ? console.log("RR") : null;
+        currentNode.right.left ? console.log("RL") : null;
+      } else if (this.__balanceFactor(currentNode) == 2) {
+        console.log(currentNode.data + " is left heavy");
+        currentNode.left.left ? console.log("LL") : null;
+        currentNode.left.right ? console.log("LR") : null;
+      }
+      currentNode = currentNode.parent;
+    }
+    if (this.__balanceFactor() == -2) {
+      console.log("root is right heavy");
+    } else if (this.__balanceFactor() == 2) {
+      console.log("root is left heavy");
+    }
   };
 }
 
 var avlTree = new AVL();
 avlTree.insertNode(5);
 avlTree.insertNode(8);
+avlTree.insertNode(4);
+avlTree.insertNode(3);
 avlTree.insertNode(2);
-avlTree.insertNode(6);
+avlTree.insertNode(1);
 avlTree.insertNode(10);
+avlTree.insertNode(14);
+avlTree.insertNode(13);
+avlTree.insertNode(6);
+avlTree.insertNode(7);
 
 // console.log(avlTree.root);
